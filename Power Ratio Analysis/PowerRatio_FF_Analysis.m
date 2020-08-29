a067dccc637afa838ee558e3d405d8624e78d95d
Sid <39922327+ranasid17@users.noreply.github.com>
2020-08-28 22:48:48 -0500

    Add files via upload

---
+++ Power Ratio Analysis/PowerRatio_FF_Analysis.m
@@ -0,0 +1,172 @@
+% Script overview:
+%   1) Find wavelet span from fwhm, fwhm from span using self-defined functions
+%   2) Enter for loop #1 (each loop == 1 pt) 
+%       a) Extract pt data into matrix, stratify by ipsi or contralesional recording, apply FFT(*)
+%       b) Extract Gabor wavelet using self-defined function, use this to calculate power envelope
+%   3) Calculate median laterality for early and late Rest and Task data for all Pts 
+%   5) Calculate change in median centroid location "                               " 
+
+% IMPORTANT: (CF, FWHM) are pt-dependent for feature frequencies. So instead of defining one for 
+%   entire run, script will iterate thru all 10 when processing each pt.
+
+% Define center frq and wavelet width
+cf = [15,16,19,11,11,9,15,11,11,17];
+fwhm = 1;
+
+% Find wavelet span (in time domain)
+[span] = fwhm2span(cf, fwhm); %fwhm2span.m
+[fwhm] = span2fwhm(cf, span); %span2fwhm.m
+
+% Preallocate outside loop to previde overriding w 0s
+ratio_RestFirst_ftfreq= zeros(150,10);
+ratio_RestLast_ftfreq = zeros(150,10);
+ratio_MoveFirst_ftfreq = zeros(150, 10);
+ratio_MoveLast_ftfreq = zeros(150, 10);
+
+%purpose: prep data (ipsi, contra), wavelet analysis, median calcs
+%(first/last + rest/move, ipsi, contra)
+for i = 1:10
+    % Step 1: Data Prep
+    %%CreateArrayforPCA -> PCA -> CalcCentroid for 10 patients (unpooled data)
+    Cell1a = FullRawRestMat{i}; % 30x2000x2x260
+    Cell1b = FullRawTaskMat{i}; % 30x2000x2x260
+    % exceptions for runs to count in early vs late analyses
+    cur_subj = ptID(i);
+    runsToCount = getRuns2Count(cur_subj,Cell1a);
+    %%constrain cell1 to FIRST 5 sessions, ipsi + contra
+    ipsiA = Cell1a(:, :, ipsi(i), runsToCount(1:5)); %First 5 REST sessions w no 0s, ipsi
+    ipsiB = Cell1b(:, :, ipsi(i), runsToCount(1:5)); %"     " TASK "                    "
+    %%constrain cell to LAST 5 sessions, ipsi + contra
+    ipsiC = Cell1a(:, :, ipsi(i), runsToCount(6:10)); %Last 5 REST sessions w no 0s, ipsi
+    ipsiD = Cell1b(:, :, ipsi(i), runsToCount(6:10)); %"    " TASK "                          "
+    
+    %%remove singleton dim, change data order, FFT for IPSILATERAL HEMISPHERE
+    ipsiA = squeeze(ipsiA); ipsiA = permute(ipsiA,[2 1 3]); %fftIpsiA = fft(ipsiA);
+    ipsiB = squeeze(ipsiB); ipsiB = permute(ipsiB, [2 1 3]); %fftIpsiB = fft(ipsiB);
+    ipsiC = squeeze(ipsiC); ipsiC = permute(ipsiC, [2 1 3]); %fftIpsiC = fft(ipsiC);
+    ipsiD = squeeze(ipsiD); ipsiD = permute(ipsiD, [2 1 3]); %fftIpsiD = fft(ipsiD);
+    
+    %%allocate memory
+    dataFirstIpsi = zeros(2000, 300);
+    dataLastIpsi = zeros(2000, 300);
+    % Step 1b: fill array w FFT data
+    a = 1;
+    while (a<=300)
+        for j = 1:5
+            %Fill REST sessions (FIRST + LAST), ipsi
+            dataFirstIpsi(:, a:a+29) = ipsiA(:, :, j);
+            dataLastIpsi(:, a:a+29) = ipsiC(:, :, end-5+j);
+            a = a + 30;
+        end
+        for j = 1:5
+            % Fill MOVE 5 sessions (FIRST + LASTT), ipsI
+            dataFirstIpsi(:, a:a+29, :) = ipsiB(:, :, j);
+            dataLastIpsi(:, a:a+29) = ipsiD(:, :, end-5+j);
+            a = a + 30;
+        end
+    end
+    % Step 2: Wavelet analysis + median calc
+    % Gabor wavelet function, ipsi (output is complex signal)
+    [gabor_responseFirst_ipsi] = gabor_response_span(dataFirstIpsi, cf, span, Fs);
+    [gabor_responseLast_ipsi] = gabor_response_span(dataLastIpsi, cf, span, Fs);
+    %Power envolope, ipsi
+    power_envFirst_ipsi = squeeze(abs(gabor_responseFirst_ipsi).^2);
+    power_envLast_ipsi = squeeze(abs(gabor_responseLast_ipsi).^2);
+    
+    %%Separate alpha power data into REST and MOVE (FIRST 5 sessions, ipsi)
+    REST_DataFirst_ipsi = power_envFirst_ipsi(:, 1:150);
+    MOVE_DataFirst_ipsi = power_envFirst_ipsi(:, 151:300);
+    %%Calc median of each REST and MOVE COLUMN
+    medianRestF_ipsi = median(REST_DataFirst_ipsi);
+    medianMoveF_ipsi = median(MOVE_DataFirst_ipsi);
+    
+    %%REPEAT for LAST 5 sessions, ipsi
+    %%Separate alpha power data into REST and MOVE (LAST 5)
+    restDataLast_ipsi = power_envLast_ipsi(:, 1:150);
+    moveDataLast_ipsi = power_envLast_ipsi(:, 151:300);
+    %Calc median of each REST and MOVE column
+    medianRestL_ipsi = median(restDataLast_ipsi);
+    medianMoveL_ipsi = median(moveDataLast_ipsi);
+    % END MEDIAN REST/MOVE CALCULATION FOR FIRST AND LAST 5 TRIALS
+    
+    %% REPEAT FOR CONTRA DATA
+    
+    % Step 1: Data Prep
+    %%CreateArrayforPCA -> PCA -> CalcCentroid for 10 patients (unpooled data)
+    Cell2a = FullRawRestMat{i}; % 30x2000x2x260
+    Cell2b = FullRawTaskMat{i}; % 30x2000x2x260
+    % exceptions for runs to count in early vs late analyses
+    cur_subj = ptID(i);
+    runsToCount = getRuns2Count(cur_subj,Cell2a);
+    %%constrain cell1 to FIRST 5 sessions, contra
+    contraA = Cell2a(:, :, contra(i), runsToCount(1:5)); %First 5 REST sessions w no 0s, contra
+    contraB = Cell2b(:, :, contra(i), runsToCount(1:5)); %"     " TASK "                "
+    %%constrain cell to LAST 5 sessions, contra
+    contraC = Cell2a(:, :, contra(i), runsToCount(6:10)); %Last 5 REST sessions w no 0s, contra
+    contraD = Cell2a(:, :, contra(i), runsToCount(6:10)); %"    " TASK "                      "
+    
+    %%remove singleton dim, change data order, FFT for CONTRALATERAL HEMISPHERE
+    
+    contraA = squeeze(contraA); contraA = permute(contraA,[2 1 3]); %fftContraA = fft(contraA);
+    contraB = squeeze(contraB); contraB = permute(contraB,[2 1 3]); %fftContraB = fft(contraB);
+    contraC = squeeze(contraC); contraC = permute(contraC,[2 1 3]); %fftContraC = fft(contraC);
+    contraD = squeeze(contraD); contraD = permute(contraD, [2 1 3]); %fftontraD = fft(contraD);
+    %%allocate memory
+    dataFirstContra = zeros(2000,300);
+    dataLastContra = zeros(2000, 300);
+    % Step 2: fill array w FFT data
+    a = 1;
+    while (a<=300)
+        for j = 1:5
+            %Fill FIRST 5 sessions (REST + MOVE), contra
+            dataFirstContra(:, a:a+29) = contraA(:, :, j);
+            dataLastContra(:, a:a+29) = contraC(:, :, end-5+j);
+            a = a + 30;
+        end
+        for j = 1:5
+            %Fill LAST 5 sessions (REST + MOVE), contra
+            dataFirstContra(:, a:a+29, :) = contraB(:, :, j);
+            dataLastContra(:, a:a+29) = contraD(:, :, end-5+j);
+            a = a + 30;
+        end
+    end
+    % Step 3: Wavelet analysis + median calc
+    %Gabor wavelet function, contra
+    [gabor_responseFirst_contra] = gabor_response_span(dataFirstContra, cf, span, Fs);
+    [gabor_responseLast_contra] = gabor_response_span(dataLastContra, cf, span, Fs);
+    %Power envelope, contra
+    alpha_power_envFirst_contra = squeeze(abs(gabor_responseFirst_contra).^2);
+    alpha_power_envLast_contra = squeeze(abs(gabor_responseLast_contra).^2);
+    
+    %%Separate alpha power data into REST and MOVE (FIRST 5 sessions, contra)
+    REST_DataFirst_contra = alpha_power_envFirst_contra(:, 1:150);
+    MOVE_DataFirst_contra = alpha_power_envFirst_contra(:, 151:300);
+    %%Calc median of each REST and MOVE COLUMN
+    medianRestF_contra = median(REST_DataFirst_contra);
+    medianMoveF_contra = median(MOVE_DataFirst_contra);
+    
+    %%REPEAT Lines 175-179 for LAST 5 sessions, contra
+    %%Separate alpha power data into REST and MOVE (LAST 5)
+    REST_DataLast_contra = power_envLast_contra(:, 1:150);
+    MOVE_DataLast_contra = power_envLast_contra(:, 151:300);
+    %Calc median of each REST and MOVE column
+    medianRestL_contra = median(REST_DataLast_contra);
+    medianMoveL_contra = median(MOVE_DataLast_contra);
+    % END CONTRA CALCS
+    
+    %% Calc contra:ipsi power ratio for each trial (FIRST + LAST)
+    ratio_RestFirst_ContraIpsi = medianRestF_contra./medianRestF_ipsi;
+    ratio_RestLast_ContraIpsi = medianRestL_contra./medianRestL_ipsi;
+    ratio_MoveFirst_ContraIpsi = medianMoveF_contra./medianMoveF_ipsi;
+    ratio_MoveLast_ContraIpsi = medianMoveL_contra./medianMoveL_ipsi;
+    
+    % Store ratios in array
+    ratio_RestFirst_ftfreq(:, i) = ratio_RestFirst_ContraIpsi(1, :);
+    ratio_RestLast_ftfreq(:, i) = ratio_RestLast_ContraIpsi(1, :);
+    ratio_MoveFirst_ftfreq(:, i) = ratio_MoveFirst_ContraIpsi(1, :);
+    ratio_MoveLast_ftfreq(:, i) = ratio_MoveLast_ContraIpsi(1, :);
+end
+%% Calc difference in early - late medians
+ratio_RestDifference_ftfreq = ratio_RestLast_ftfreq - ratio_RestFirst_ftfreq;
+ratio_MoveDifference_ftfreq = ratio_MoveLast_ftfreq - ratio_MoveFirst_ftfreq;
+
---
+++ Power Ratio Analysis/plotPowerRatio_Analysis.m
@@ -0,0 +1,163 @@
+% Goal: 
+%   1) Plot results of 'laterality_xFrqBand.m' scripts in publishable format 
+%   2) Calculate Spearman Corr Coeff and test for significance
+
+% NOTE: This plotting script is NOT compatable with 'PowerBand_Laterality_Analysis.m' (file in this directory)! 
+%       This is to prevent unauthorized access to printed figures. Please contact author directly for 
+%       laterality_xFrqBand.m scripts which are compatable with this file. 
+%       Else use provided graphing functions in 'laterality_A.m'.
+
+clc
+
+ARATchange = [12.5, 0.5, 8, 6, 2, 6, 1, 7, 13.5, 5.5]; %arat scores
+Fit_medAllPts_Difference = zeros(150, 150);
+
+% Concatenate DIFFERENCE ratios
+allPts_Difference(:,:,1) = ratio_RestDifference_alpha(:,:);
+allPts_Difference(:,:,2) = ratio_MoveDifference_alpha(:,:);
+allPts_Difference(:,:,3) = ratio_RestDifference_beta(:,:);
+allPts_Difference(:,:,4) = ratio_MoveDifference_beta(:,:);
+allPts_Difference(:,:,5) = ratio_RestDifference_delta(:,:);
+allPts_Difference(:,:,6) = ratio_MoveDifference_delta(:,:);
+allPts_Difference(:,:,7) = ratio_RestDifference_ftfreq(:,:);
+allPts_Difference(:,:,8) = ratio_MoveDifference_ftfreq(:,:);
+allPts_Difference(:,:,9) = ratio_RestDifference_theta(:,:);
+allPts_Difference(:,:,10) = ratio_MoveDifference_theta(:,:);
+
+%This is the array plotted: median values of change in alpha/beta/delta/ft
+medAllPts_Difference = nanmedian(allPts_Difference);
+%remove singleton dim from med calc
+medAllPts_Difference = squeeze(medAllPts_Difference);
+%tpose arat matrix for calc purposes
+ARATposed = ARATchange';
+%Corr coeff + p value calc
+[rho, p] = corr(medAllPts_Difference, ARATposed,'type', 'spearman');
+
+% [r,m,b] = regression(medAllPts_Difference, ARATchange);
+
+titles = {'Change in Rest Median Lateralities, Alpha Band';
+    'Change in Move Median Lateralities, Alpha Band';
+    'Change in Rest Median Lateralities, Beta Band';
+    'Change in Move Median Lateralities, Beta Band';
+    'Change in Rest Median Lateralities, Delta Band';
+    'Change in Move Median Lateralities, Delta Band';
+    'Change in Rest Median Lateralities, Ft. Frq';
+    'Change in Move Median Lateralities, Ft. Frq';
+    'Change in Rest Median Lateralities, Theta Band';
+    'Change in Move Median Lateralities, Theta Band'};
+
+% Modified from 'Resting Power Panel Fig' (Joey)
+% Plot change in Contralateral:Ipsilateral ratio of signal from FIRST ... 
+% five trials vs. LAST five trials
+% TOP ROW: Change in Contra:Ispilat ratio when in REST state
+% BOTTOM ROW: "                                 " TASK state
+% Each column: Band frequency (alpha, beta, delta, feat, theta)
+
+% Motor - UPDATES FOR PAPER
+close all
+
+%define colors
+rd = [0.7 0.1 0.1];
+% blu = [0 0.447 0.741];
+blu = [0.301 0.745 0.933]; %actually cyan
+grn = [0.466 0.674 0.188];
+% oj = [0.85 0.325 0.098];
+oj = [0.494 0.184 0.556]; %actually purple
+
+figure
+
+%ALPHA REST med change
+subplot(2,5,3)
+[r p] = corr(medAllPts_Difference(:,1), ARATposed,'type','Spearman');
+sprintf('Contralesional Motor Electrode Relative Delta Power Change Correlation with ARAT Change: \nr = %5.4f, p = %5.4f',r,p)
+plot(medAllPts_Difference(:,1), ARATposed,'.','Color',blu,'MarkerSize',24)
+ylim([0, 14])
+title(sprintf('Alpha Band, r = %5.4f, p = %5.4f',r,p))
+
+conditional_lsline;
+
+%ALPHA MOVE med change
+subplot(2,5,8)
+[r p] = corr(medAllPts_Difference(:,2), ARATposed,'type','Spearman');
+sprintf('Contralesional Motor Electrode Relative Theta Power Change Correlation with ARAT Change: \nr = %5.4f, p = %5.4f',r,p)
+plot(medAllPts_Difference(:,2),ARATposed,'.','Color',rd,'MarkerSize',24)
+ylim([0, 14])
+title(sprintf('Alpha Band, r = %5.4f, p = %5.4f',r,p))
+
+conditional_lsline;
+
+%BETA REST med change
+subplot(2,5,4)
+[r p] = corr(medAllPts_Difference(:,3), ARATposed,'type','Spearman');
+sprintf('Contralesional Motor Electrode Relative Alpha Power Change Correlation with ARAT Change: \nr = %5.4f, p = %5.4f',r,p)
+plot(medAllPts_Difference(:,3),ARATposed,'.','Color',blu,'MarkerSize',24)
+ylim([0, 14])
+title(sprintf('Beta Band, r = %5.4f, p = %5.4f',r,p))
+conditional_lsline;
+
+%BETA MOVE med change 
+subplot(2,5,9)
+[r p] = corr(medAllPts_Difference(:,4), ARATposed,'type','Spearman');
+sprintf('Contralesional Motor Electrode Relative Beta Power Change Correlation with ARAT Change: \nr = %5.4f, p = %5.4f',r,p)
+plot(medAllPts_Difference(:,4), ARATposed,'.','Color',rd,'MarkerSize',24)
+ylim([0, 14])
+title(sprintf('Beta Band, r = %5.4f, p = %5.4f',r,p))
+conditional_lsline;
+
+%DELTA REST med change
+subplot(2,5,1)
+[r p] = corr(medAllPts_Difference(:,5), ARATposed,'type','Spearman');
+sprintf('Contralesional Motor Electrode Relative Feature Power Change Correlation with ARAT Change: \nr = %5.4f, p = %5.4f',r,p)
+plot(medAllPts_Difference(:,5), ARATposed,'.','Color',blu,'MarkerSize',24)
+ylim([0, 14])
+title(sprintf('Delta Band, r = %5.4f, p = %5.4f',r,p))
+xlabel('Rest state Contralateral:Ipsilateral Signal Change')
+ylabel('Motor Recovery (ARAT Change)')
+conditional_lsline;
+
+%DELTA MOVE med change
+subplot(2,5,6)
+[r p] = corr(medAllPts_Difference(:,6), ARATposed,'type','Spearman');
+sprintf('Ipsilesional Motor Electrode Relative Delta Power Change Correlation with ARAT Change: \nr = %5.4f, p = %5.4f',r,p)
+plot(medAllPts_Difference(:,6), ARATposed,'.','Color',rd,'MarkerSize',24)
+ylim([0, 14])
+title(sprintf('Delta Band, r = %5.4f, p = %5.4f',r,p))
+xlabel('Task state Contralateral:Ipsilateral Signal Change')
+ylabel('Motor Recovery (ARAT Change)')
+conditional_lsline;
+
+%FT FRQ REST med change
+subplot(2,5,5)
+[r p] = corr(medAllPts_Difference(:,7), ARATposed,'type','Spearman');
+sprintf('Ipsilesional Motor Electrode Relative Theta Power Change Correlation with ARAT Change: \nr = %5.4f, p = %5.4f',r,p)
+plot(medAllPts_Difference(:,7), ARATposed, '.','Color',blu,'MarkerSize',24)
+ylim([0, 14])
+title(sprintf('Feat. Freq, r = %5.4f, p = %5.4f',r,p))
+conditional_lsline;
+
+%FT FRQ MOVE med change
+subplot(2,5,10)
+[r p] = corr(medAllPts_Difference(:,8), ARATposed,'type','Spearman');
+sprintf('Ipsilesional Motor Electrode Relative Alpha Power Change Correlation with ARAT Change: \nr = %5.4f, p = %5.4f',r,p)
+plot(medAllPts_Difference(:,8), ARATposed,'.','Color',rd,'MarkerSize',24)
+ylim([0, 14])
+title(sprintf('Feat. Freq, r = %5.4f, p = %5.4f',r,p))
+conditional_lsline;
+
+%THETA REST med change
+subplot(2,5,2)
+[r p] = corr(medAllPts_Difference(:,9), ARATposed,'type','Spearman');
+sprintf('Ipsilesional Motor Electrode Relative Theta Power Change Correlation with ARAT Change: \nr = %5.4f, p = %5.4f',r,p)
+plot(medAllPts_Difference(:,9), ARATposed, '.','Color',blu,'MarkerSize',24)
+ylim([0, 14])
+title(sprintf('Theta Band, r = %5.4f, p = %5.4f',r,p))
+conditional_lsline;
+
+%THETA MOVE med change
+subplot(2,5,7)
+[r p] = corr(medAllPts_Difference(:,10), ARATposed,'type','Spearman');
+sprintf('Ipsilesional Motor Electrode Relative Theta Power Change Correlation with ARAT Change: \nr = %5.4f, p = %5.4f',r,p)
+plot(medAllPts_Difference(:,10), ARATposed,'.','Color',rd,'MarkerSize',24)
+ylim([0, 14])
+title(sprintf('Theta Band, r = %5.4f, p = %5.4f',r,p))
+conditional_lsline;
