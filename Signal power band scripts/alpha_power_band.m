4e540ca4a11d6867f8afb51ca084d0aa1fafafd2
Sid <39922327+ranasid17@users.noreply.github.com>
2020-08-28 19:23:27 -0500

    Update alpha_power_band.m

--- Signal power band scripts/alpha_power_band.m
+++ Signal power band scripts/alpha_power_band.m
@@ -1,34 +1,12 @@
-% %%Merge 'createArrayForPCA', 'PCAFunction', 'centroidCalculation'
-% %%Run same processes from above scripts on Patients 1-10 for 1st 5 trials
-% %%Constrict to ALPHA BAND only
+% Script overview:
+%   1) Merge 'createArrayForPCA', 'PCAFunction', 'centroidCalculation'
+%   2) Run PCA on early and late MR, MT data for Patients 1-10 
+%   3) Calculate average centroids from PCA results " " 
+%   4) Calculate change in avg centroid location " " 
+%   5) Constrict to ALPHA BAND only
 %
-% %%createArrayForPCA
-%clear all
-clc
-close all
-% %%load data
-load('D:\Sid\Data\MotorRest.mat')
-load('D:\Sid\Data\MotorTask.mat')
-%% Remove empty cells from REST data
-emptyCell = false(numel(FullRawRestMat),1);
-for i = 1:numel(FullRawRestMat)
-    if isempty(FullRawRestMat{i})
-        emptyCell(i) = true;
-    end
-end
-FullRawRestMat(emptyCell) = [];
-%%Remove empty cells from MOVE data
-emptyCell = false(numel(FullRawTaskMat),1);
-for i = 1:numel(FullRawTaskMat)
-    if isempty(FullRawTaskMat{i})
-        emptyCell(i) = true;
-    end
-end
-FullRawTaskMat(emptyCell) = [];
-goodCell = ~emptyCell;
-ptID = find(goodCell);
 
-%%
+%% Step 2: PCAFunction, ipsilesional electrode
 clc
 % parameters
 Fs = 256; %sampling frequency
@@ -36,8 +14,9 @@ T = 1/Fs; %sampling frequency
 L = 2000; %length of signal
 t = (0:L-1)*T; %time vector
 
-% Given by joey
+% Motor scores
 ARATchange = [12.5 0.5 8 6 2 6 1 7 13.5 5.5];
+% Define ipsi/contralesional electrodes
 ipsi = [2,1,2,2,2,1,1,1,2,1];
 contra = [1,2,1,1,1,2,2,2,1,2];
 
@@ -56,9 +35,9 @@ centDist_alpha_ipsi = zeros(size(1, 10));
 figure()
 for i = 1:10
     % Step 1: Data Prep
-    %%CreateArrayforPCA -> PCA -> CalcCentroid for 10 patients (unpooled data)
-    Cell1a = FullRawRestMat{i}; % 30x2000x2x260
-    Cell1b = FullRawTaskMat{i}; % 30x2000x2x260
+    % CreateArrayforPCA -> PCA -> CalcCentroid for 10 patients (unpooled data)
+    Cell1a = MotorRest{i}; % 30x2000x2x260
+    Cell1b = MotorTask{i}; % 30x2000x2x260
     % exceptions for runs to count in early vs late analyses
     cur_subj = ptID(i);
     runsToCount = getRuns2Count(cur_subj,Cell1a);
@@ -84,14 +63,14 @@ for i = 1:10
     while (a<=300)
         for j = 1:5
             %Fill REST sessions (FIRST + LAST), ipsi
-            dataFirstIpsi(:, a:a+29) = ipsiA(:, :, j);
-            dataLastIpsi(:, a:a+29) = ipsiC(:, :, end-5+j);
+            dataFirstIpsi(:, a:a+29) = fftIpsiA(:, :, j);
+            dataLastIpsi(:, a:a+29) = fftIpsiC(:, :, end-5+j);
             a = a + 30;
         end
         for j = 1:5
             % Fill MOVE 5 sessions (FIRST + LASTT), ipsI
-            dataFirstIpsi(:, a:a+29, :) = ipsiB(:, :, j);
-            dataLastIpsi(:, a:a+29) = ipsiD(:, :, end-5+j);
+            dataFirstIpsi(:, a:a+29, :) = fftIpsiB(:, :, j);
+            dataLastIpsi(:, a:a+29) = fftIpsiD(:, :, end-5+j);
             a = a + 30;
         end
     end
@@ -103,12 +82,12 @@ for i = 1:10
     alpha_power_envFirst_ipsi = squeeze(abs(gabor_responseFirst_ipsi).^2);
     alpha_power_envLast_ipsi = squeeze(abs(gabor_responseLast_ipsi).^2);
     
-    %% PCA
+    % PCA
     % ipsi data
     [coeffF_ipsi, scoreF_ipsi, latentF_ipsi, tsquaredF_ipsi, explainedF_ipsi] = pca(alpha_power_envFirst_ipsi);
     [coeffL_ipsi, scoreL_ipsi, latentL_ipsi, tsquaredL_ipsi, explainedL_ipsi] = pca(alpha_power_envLast_ipsi);
     
-    %% Find vars needed to explain 90% variance in each PCA function
+    % Find vars needed to explain 90% variance in each PCA function
     % FIRST 5 trials, ipsi
     EF_ipsi = zeros(size(explainedF_ipsi));
     for j = 1:numel(explainedF_ipsi)
@@ -129,7 +108,7 @@ for i = 1:10
     end
     EL_ipsi(EL_ipsi == 0) = []; %delete extra 0s
   
-    %% Plotting centroids, ipsi
+    % Plotting centroids, ipsi
     % Center graph axes
     ax = gca;
     ax.XAxisLocation = 'origin'; %move X AXIS location to origin
@@ -148,9 +127,9 @@ for i = 1:10
     meanRestF_ipsi = meanRestF_ipsi(1:numel(EF_ipsi));
     meanMoveF_ipsi = meanMoveF_ipsi(1:numel(EF_ipsi));
     
-    %%REPEAT for LAST 5 sessions, ipsi
+    % REPEAT for LAST 5 sessions, ipsi
     
-    %%Separate PCA data into REST and MOVE (LAST 5)
+    % Separate PCA data into REST and MOVE (LAST 5)
     restDataPCALast_ipsi = scoreL_ipsi(1:150, :);
     moveDataPCALast_ipsi = scoreL_ipsi(151:300, :);
     %%Calc avg of each REST and MOVE COLUMN
@@ -160,24 +139,24 @@ for i = 1:10
     meanRestL_ipsi = meanRestL_ipsi(1:numel(EL_ipsi));
     meanMoveL_ipsi = meanMoveL_ipsi(1:numel(EL_ipsi));
     
-    %%Calc distance between centroids
+    % Calc distance between centroids
     distanceF_ipsi = norm(meanRestF_ipsi - meanMoveF_ipsi);
     distanceL_ipsi = norm(meanRestL_ipsi - meanMoveL_ipsi);
-    %%Store calc'd distance in centDist
+    % Store calc'd distance in centDist
     centDist_alpha_temp_ipsi(1, i) = distanceF_ipsi; %FIRST 5 sessions = Row 1
     centDist_alpha_temp_ipsi(2, i) = distanceL_ipsi; %LAST 5 sessions = Row 2
     %%Calc change from FIRST to LAST 5 sessions
     centDist_alpha_ipsi(i) = centDist_alpha_temp_ipsi(2, i) - centDist_alpha_temp_ipsi(1, i);
     
-    %Plot change in ARAT against change in centroid
+    % Plot change in ARAT against change in centroid
     scatter(centDist_alpha_ipsi(i), ARATchange(i))
     title('Avg change in ipsilat alpha band position v. ARAT change')
     
 end
 hold off
 
-%%
-% REPEAT FOR LOOP FOR CONTRA DATA
+%% Step 2: contralesional electrode
+% REPEAT STEP 1 FOR LOOP FOR CONTRA DATA
 % Preallocate outside loop to previde overriding w 0s
 centDist_alpha_temp_contra = zeros(size(200,20)); 
 centDist_alpha_contra = zeros(size(1, 10)); 
@@ -185,51 +164,51 @@ centDist_alpha_contra = zeros(size(1, 10));
 figure()
 for i = 1:10
     % Step 1: Data Prep
-    %%CreateArrayforPCA -> PCA -> CalcCentroid for 10 patients (unpooled data)
-    Cell1a = FullRawRestMat{i}; % 30x2000x2x260
-    Cell1b = FullRawTaskMat{i}; % 30x2000x2x260
+    % CreateArrayforPCA -> PCA -> CalcCentroid for 10 patients (unpooled data)
+    Cell1a = MotorRest{i}; % 30x2000x2x260
+    Cell1b = MotorTask{i}; % 30x2000x2x260
     % exceptions for runs to count in early vs late analyses
     cur_subj = ptID(i);
     runsToCount = getRuns2Count(cur_subj,Cell1a);
-    %%constrain cell1 to FIRST 5 sessions, contra
+    % constrain cell1 to FIRST 5 sessions, contra
      contraA = Cell1a(:, :, contra(i), runsToCount(1:5)); %First 5 REST sessions w no 0s, contra
      contraB = Cell1b(:, :, contra(i), runsToCount(1:5)); %"     " TASK "                "
-    %%constrain cell to LAST 5 sessions, contra
+    % constrain cell to LAST 5 sessions, contra
      contraC = Cell1a(:, :, contra(i), runsToCount(6:10)); %Last 5 REST sessions w no 0s, contra
      contraD = Cell1a(:, :, contra(i), runsToCount(6:10)); %"    " TASK "                      "
     
-    %%remove singleton dim, change data order, FFT for CONTRALATERAL HEMISPHERE
+    % remove singleton dim, change data order, FFT for CONTRALATERAL HEMISPHERE
     contraA = squeeze(contraA); contraA = permute(contraA,[2 1 3]); fftContraA = fft(contraA);
     contraB = squeeze(contraB); contraB = permute(contraB,[2 1 3]); fftContraB = fft(contraB);
     contraC = squeeze(contraC); contraC = permute(contraC,[2 1 3]); fftContraC = fft(contraC);
-    contraD = squeeze(contraD); contraD = permute(contraD, [2 1 3]); fftontraD = fft(contraD);
+    contraD = squeeze(contraD); contraD = permute(contraD, [2 1 3]); fftContraD = fft(contraD);
     %%allocate memory
     dataFirstContra = zeros(2000,300);
     dataLastContra = zeros(2000, 300);
-    %%fill array w FFT data
+    % fill array w FFT data
     a = 1;
     while (a<=300)
         for j = 1:5
             %Fill FIRST 5 sessions (REST + MOVE), contra
-            dataFirstContra(:, a:a+29) = contraA(:, :, j);
-            dataLastContra(:, a:a+29) = contraC(:, :, end-5+j);
+            dataFirstContra(:, a:a+29) = fftContraA(:, :, j);
+            dataLastContra(:, a:a+29) = fftContraC(:, :, end-5+j);
             a = a + 30;
         end
         for j = 1:5
             %Fill LAST 5 sessions (REST + MOVE), contra
-            dataFirstContra(:, a:a+29, :) = contraB(:, :, j);
-            dataLastContra(:, a:a+29) = contraD(:, :, end-5+j);
+            dataFirstContra(:, a:a+29, :) = fftContraB(:, :, j);
+            dataLastContra(:, a:a+29) = fftContraD(:, :, end-5+j);
             a = a + 30;
         end
     end
-    %Gabor wavelet function, contra
+    % Gabor wavelet function, contra
     [gabor_responseFirst_contra] = gabor_response_span(dataFirstContra, cf, span, Fs);
     [gabor_responseLast_contra] = gabor_response_span(dataLastContra, cf, span, Fs);
-    %Power envelope, contra
+    % Power envelope, contra
     alpha_power_envFirst_contra = squeeze(abs(gabor_responseFirst_contra).^2);
     alpha_power_envLast_contra = squeeze(abs(gabor_responseLast_contra).^2);
     
-    %% PCA
+    % PCA
     % ipsi data
     [coeffF_ipsi, scoreF_ipsi, latentF_ipsi, tsquaredF_ipsi, explainedF_ipsi] = pca(alpha_power_envFirst_ipsi);
     [coeffL_ipsi, scoreL_ipsi, latentL_ipsi, tsquaredL_ipsi, explainedL_ipsi] = pca(alpha_power_envLast_ipsi);
@@ -257,7 +236,7 @@ for i = 1:10
     end
     EL_contra(EL_contra == 0) = []; %delete extra 0s
     
-    %% Plotting centriods
+    % Plotting centriods
     % Center graph axes
     ax = gca;
     ax.XAxisLocation = 'origin'; %move X AXIS location to origin
